9e7ba0521d00d045cdc0bad001e8b2f4457f92e9
shzy2012 <shzy2012@gmail.com>
2019-09-25 15:20:06 +0800

    Create wulai.rb

---
+++ wulai.rb
@@ -0,0 +1,28 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Wulai < Formula
+  desc "wulai bot client with cmdline"
+  homepage "https://github.com/shzy2012/wulai"
+  url "https://github.com/shzy2012/wulai/releases/download/v1.0.0/wulai.tar.gz"
+  sha256 "79e4ddf6ea445cbb27d4e365ab102ce8351941d0635e3bb301241f1251bc0b99"
+
+  # depends_on "cmake" => :build
+
+  def install
+	  bin.install "wulai"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test wulai`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
